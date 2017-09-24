6dc78cc9bb702e261a98c259d4d9fc6aa71593e7
Stephan Kempa <stephan@carr-ireland.com>
2017-09-24 14:35:44 +0100

    Inserted some more Rune Effects

--- src/main/java/thewizardmod/books/GuiRuneMagic.java
+++ src/main/java/thewizardmod/books/GuiRuneMagic.java
@@ -55,35 +55,35 @@ public class GuiRuneMagic extends GuiScreen {
 
 		stringPageText[0] = "   Rune Magic Guide";
 
-		stringPageText[1] = "Page 1\n §6Text here.";
+		stringPageText[1] = "Magic of Runes is a secrect, that was given from father to son over ages. It could bring you more then you could expect, but it also can be very dangerous.";
 
-		stringPageText[2] = "Page 2\n Text here.";
+		stringPageText[2] = "You can apply Runes to Rune Stones around an altar, or just for other use by right clicking a rune stone with the rune.\n\nAll runes are made on the altar.";
 		stringPageText[3] = "Blank Rune\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust";
 		stringPageText[4] = "All Other Runes will be made with\n 1 Shadow Dust\n 1 Blank Rune\n 1 Magic Gem\n\nPlus one extra increment, like listet on the next sides.";
-		stringPageText[5] = "Rune of Luck\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Gold Ingot";
-		stringPageText[6] = "Rune of Power\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Blaze Powder";
-		stringPageText[7] = "Rune of Fire\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Fire Charger";
-		stringPageText[8] = "Rune of knowledge\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Book";
-		stringPageText[9] = "Rune of growth\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Cherry Tree Sappling";
-		stringPageText[10] = "Rune of Beast\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Spider Eye";
-		stringPageText[11] = "Rune of Keeping\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Chest";
-		stringPageText[12] = "Rune of Sign\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Banner";
-		stringPageText[13] = "Rune of Attack\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Arrow";
-		stringPageText[14] = "Rune of Earth\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Coal";
-		stringPageText[15] = "Rune of Ice\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Ice";
-		stringPageText[16] = "Rune of Plants\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Magic Seed";
-		stringPageText[17] = "Rune of Cloth\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Magic String";
-		stringPageText[18] = "Rune of Sun\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Rune of Fire";
-		stringPageText[19] = "Rune of Defence\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Shild";
-		stringPageText[20] = "Rune of Weather\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Nether Star";
-		stringPageText[21] = "Rune of the Rich\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Rune of Luck";
-		stringPageText[22] = "Rune of Food\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Apple";
-		stringPageText[23] = "Rune of Nature\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Rune of Knowledge";
-		stringPageText[24] = "Rune of Poison\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Poison Potato";
-		stringPageText[25] = "Rune of View\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Ender Eye";
-		stringPageText[26] = "Rune of Air\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Feather";
-		stringPageText[27] = "Rune of Rest\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Bed";
-		stringPageText[28] = "Rune of Energy\n\nIncrements\n 1 Stone\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Flint and Steel";
+		stringPageText[5] = "Rune of Luck\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Gold Ingot";
+		stringPageText[6] = "Rune of Power\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Blaze Powder";
+		stringPageText[7] = "Rune of Fire\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Fire Charger";
+		stringPageText[8] = "Rune of knowledge\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Book";
+		stringPageText[9] = "Rune of growth\n\nLet Plants around growth\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Cherry Tree Sappling";
+		stringPageText[10] = "Rune of Beast\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Spider Eye";
+		stringPageText[11] = "Rune of Keeping\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Chest";
+		stringPageText[12] = "Rune of Sign\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Banner";
+		stringPageText[13] = "Rune of Attack\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Arrow";
+		stringPageText[14] = "Rune of Earth\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Coal";
+		stringPageText[15] = "Rune of Ice\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Ice";
+		stringPageText[16] = "Rune of Plants\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Magic Seed";
+		stringPageText[17] = "Rune of Cloth\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Magic String";
+		stringPageText[18] = "Rune of Sun\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Rune of Fire";
+		stringPageText[19] = "Rune of Defence\n\nTeleports Mobs around 20 Blocks up\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Shild";
+		stringPageText[20] = "Rune of Weather\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Nether Star";
+		stringPageText[21] = "Rune of the Rich\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Rune of Luck";
+		stringPageText[22] = "Rune of Food\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Apple";
+		stringPageText[23] = "Rune of Nature\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Rune of Knowledge";
+		stringPageText[24] = "Rune of Poison\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Poison Potato";
+		stringPageText[25] = "Rune of Health\n\nStand on it for Regeneration\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Ender Eye";
+		stringPageText[26] = "Rune of Air\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Feather";
+		stringPageText[27] = "Rune of Rest\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Bed";
+		stringPageText[28] = "Rune of Energy\n\nIncrements\n 1 Blank Rune\n 1 Shadow Dust\n 1 Magic Gem\n 1 Redstone Dust\n 1 Flint and Steel";
 	}
 
 	/**
--- src/main/java/thewizardmod/runeSlab/TileEntityRuneSlab.java
+++ src/main/java/thewizardmod/runeSlab/TileEntityRuneSlab.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
@@ -117,7 +118,6 @@ public class TileEntityRuneSlab extends TileEntity implements ITickable{
 					int chance = random.nextInt(1500);
 					if(chance == 0)
 					{
-//						System.out.println("Appling bonemeal to " + newpos.getX() + " " + newpos.getY() + " " + newpos.getZ());
 						ItemDye.applyBonemeal(new ItemStack(Items.DYE, 15, 0), worldObj, newpos, null);
 					}
 				}
@@ -148,6 +148,23 @@ public class TileEntityRuneSlab extends TileEntity implements ITickable{
 		}
 	}
 
+	public void handleHealth() {
+		BlockPos centerOfField = this.getPos();
+
+		double posX = centerOfField.getX();
+		double posY = centerOfField.getY();
+		double posZ = centerOfField.getZ();
+		if (!worldObj.isRemote) {
+			for (Object obj : worldObj.getEntitiesWithinAABBExcludingEntity(null, new AxisAlignedBB(posX - 0.5D, posY, posZ - 0.5D, posX + 0.5D, posY + 1, posZ + 0.5D))) {
+				if (obj instanceof EntityPlayer) 
+				{
+					EntityPlayer player = (EntityPlayer) obj;
+					player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 20));
+					player.addPotionEffect(new PotionEffect(MobEffects.SATURATION, 20));
+				}
+			}
+		}
+	}
 	@Override
 	public void update() {
 //		System.out.println(runeType);
@@ -158,6 +175,10 @@ public class TileEntityRuneSlab extends TileEntity implements ITickable{
 		if(runeType == 15)		// rune of defence
 		{
 			handleDefence();
+		}
+		if(runeType == 20)		// rune of health
+		{
+			handleHealth();
 		}
 	}
 
